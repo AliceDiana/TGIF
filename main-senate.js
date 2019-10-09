17c30d119ba742f1b97e3012907d0dba5756d852
Alice Diana <aliceangela89@gmail.com>
2019-10-09 13:28:51 +0200

    firstcomment

---
+++ main-senate.js
@@ -0,0 +1,39 @@
+console.log(data.results[0].members);
+let members = data.results[0].members;
+
+function generateTable(membersArray) {
+  var tbody = document.getElementById("senate-data");
+
+  for (var i = 0; i < membersArray.length; i++) {
+    let row = document.createElement("tr");
+    let name = document.createElement("td");
+    let party = document.createElement("td");
+    let state = document.createElement("td");
+    let years = document.createElement("td");
+    let votes = document.createElement("td");
+
+    // name.innerHTML =
+    //   "<a href='" +
+    //   membersArray[i].url +
+    //   "' target='_blank'>" +
+    //   membersArray[i].last_name +
+    //   ", " +
+    //   membersArray[i].first_name +
+    //   " " +
+    //   (membersArray[i].middle_name || " ") +
+    //   "</a>";
+
+    name.innerHTML = `<a href= "${membersArray[i].url}" target= "_blank"> ${
+      membersArray[i].last_name
+    }, ${membersArray[i].first_name} ${membersArray[i].middle_name || " "}</a>`; // || if there is no middle name display empty
+    party.innerHTML = membersArray[i].party;
+    state.innerHTML = membersArray[i].state;
+    years.innerHTML = membersArray[i].seniority;
+    votes.innerHTML = membersArray[i].votes_with_party_pct + "%";
+
+    row.append(name, party, state, years, votes);
+    tbody.append(row);
+  }
+}
+
+generateTable(members);
